4db27868afcc923d90faf86141ad3e7c9906d884
Zagaki <Zagaki@gmail.com>
2016-05-18 18:49:48 -0700

    prep for adding in client

--- a2/a2/Start.java
+++ a2/a2/Start.java
@@ -15,39 +15,6 @@ public class Start
         // -Dsun.java2d.d3d=false sf1 57909     // server and 1 client
         // -Dsun.java2d.d3d=false offline       // no network
 
-        if (args.length < 2)
-            return;
-
-        if (args[1].contains("server"))
-        {
-            int port = Integer.parseInt(args[2]);
-            System.out.println("starting server using port " + port);
-            new GameServerTCP(port);
-        }
-        else if (args[1].contains("client"))
-        {
-            InetAddress address = InetAddress.getByName(args[2]);
-            int port = Integer.parseInt(args[3]);
-            System.out.println("starting client with connection " + address.getHostAddress() + ":" + port);
-            new DungeonCrawler3DSoonTM(address, port).start();
-        }
-        else if (args[1].contains("sf1"))
-        {
-            int port = Integer.parseInt(args[2]);
-            InetAddress address = InetAddress.getByName("localhost");
-
-            System.out.println("starting server using port " + port);
-            GameServerTCP server = new GameServerTCP(port);
-
-            System.out.println("starting client with connection " + address.getHostAddress() + ":" + port);
-            new DungeonCrawler3DSoonTM(address, port, server).start();
-        }
-        else if (args[1].contains("offline"))
-        {
-            System.out.println("starting offline client");
-            new DungeonCrawler3DSoonTM().start();
-        }
-        else
-            System.out.println("bad args");
+        new DungeonCrawler3DSoonTM().start();
     }
 }
